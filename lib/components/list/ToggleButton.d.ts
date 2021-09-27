5d8236c5d32fa83cfae565883f0672d9b98fde85
Brian Rollins <brian_rollins@fd.org>
2021-09-27 21:09:22 +0000

    removed comma from render method of ToggleButton && rolled reactivecore to 9.12.1

---
+++ lib/components/list/ToggleButton.d.ts
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { CommonProps } from '../..';
+import * as types from '../../types';
+
+export interface ToggleButtonProps extends CommonProps {
+	data?: types.data;
+	dataField: string;
+	defaultValue?: types.stringOrArray;
+	value?: types.stringOrArray;
+	onValueChange?: (...args: any[]) => any;
+	onChange?: (...args: any[]) => any;
+	filterLabel?: string;
+	innerClass?: types.style;
+	multiSelect?: boolean;
+	nestedField?: string;
+	react?: types.react;
+	showFilter?: boolean;
+	title?: types.title;
+	index?: string;
+	group?: string;
+}
+
+declare const ToggleButton: React.ComponentClass<ToggleButtonProps>;
+
+export default ToggleButton;
